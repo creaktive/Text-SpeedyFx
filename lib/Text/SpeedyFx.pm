d88b5ece60fc466bc7d30ff38dcf5ff9986310e3
Stanislaw Pusep <stanislav.poussep@buscapecompany.com>
2012-03-28 17:24:31 -0300

    passes release tests

--- lib/Text/SpeedyFx.pm
+++ lib/Text/SpeedyFx.pm
@@ -1,24 +1,17 @@
 package Text::SpeedyFx;
-# ABSTRACT: SpeedyFx
+# ABSTRACT: Extremely Fast Text Feature Extraction for Classification and Indexing
 
-use 5.010001;
 use strict;
+use utf8;
 use warnings;
 
 require Exporter;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Text::SpeedyFx ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = (all => []);
-our @EXPORT_OK = (@{$EXPORT_TAGS{all}});
-our @EXPORT = qw();
+#our %EXPORT_TAGS = (all => []);
+#our @EXPORT_OK = (@{$EXPORT_TAGS{all}});
+#our @EXPORT = qw();
 
 # VERSION
 
@@ -26,3 +19,27 @@ require XSLoader;
 XSLoader::load('Text::SpeedyFx', $VERSION);
 
 1;
+__END__
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 REFERENCES
+
+=for :list
+* L<Extremely Fast Text Feature Extraction for Classification and Indexing|http://www.hpl.hp.com/techreports/2008/HPL-2008-91R1.pdf> by L<George Forman|http://www.hpl.hp.com/personal/George_Forman/> and L<Evan Kirshenbaum|http://www.kirshenbaum.net/evan/index.htm>
+
+=method new
+
+...
+
+=method hash
+
+...
+
+=cut
--- t/00-load.t
+++ t/00-load.t
@@ -1,5 +1,11 @@
 use strict;
+use utf8;
 use warnings;
 
 use Test::More tests => 1;
-BEGIN { use_ok('Text::SpeedyFx') };
+
+BEGIN {
+    use_ok(q(Text::SpeedyFx));
+};
+
+diag(qq(Testing Text::SpeedyFx v$Text::SpeedyFx::VERSION, Perl $], $^X));
