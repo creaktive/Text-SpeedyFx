b146f6c370b6e84d9932f49ed9b7c2e23d66a1e1
Stanislaw Pusep <creaktive@gmail.com>
2012-12-27 19:19:04 -0200

    documented multibyte parsing tricks

--- lib/Text/SpeedyFx.pm
+++ lib/Text/SpeedyFx.pm
@@ -41,6 +41,29 @@ XS implementation of a very fast combined parser/hasher which works well on a va
 
 L<Original implementation|http://www.hpl.hp.com/techreports/2008/HPL-2008-91R1.pdf> is in Java and was adapted for a better Unicode compliance.
 
+=head1 UNICODE SUPPORT
+
+Due to the nature of Perl, Unicode support is handled differently from the original implementation.
+By default, L<Text::SpeedyFx> recognizes UTF-8 encoded code points in the range I<00000-2FFFF>:
+
+=for :list
+* B<Plane 0>, the B<Basic Multilingual Plane> (BMP, I<0000–FFFF>)
+* B<Plane 1>, the B<Supplementary Multilingual Plane> (SMP, I<10000–1FFFF>)
+* B<Plane 2>, the B<Supplementary Ideographic Plane> (SIP, I<20000–2FFFF>)
+* There are planes up to 16; however, as in Perl v5.16.2, there are no code points matching C<isALNUM_utf8()> there (so it's irrelevant for proper algorithm operation).
+
+Although, there is a major drawback: in this mode, B<each instance> allocates up to 1 MB of memory.
+
+If the application doesn't need to support code points beyond the B<Plane 0>
+(like the original SpeedyFx implementation) it is possible to constraint the address space to 16 bits, which lowers memory allocation to up to 256 KB.
+In fact, L<Text::SpeedyFx> constructor accepts bit range between 8 and 18 to address code points.
+
+=head2 LATIN-1 SUPPORT
+
+8 bit address space has one special meaning: it completely disables multibyte support.
+In 8 bit mode, each instance will only allocate 256 bytes and hashing will be run up to 66% faster.
+Tokenization will fallback to I<ISO 8859-1 West European languages (Latin-1)> character definitions.
+
 =method new([$seed, $bits])
 
 Initialize parser/hasher, can be customized with the options:
@@ -56,24 +79,27 @@ Hash seed (default: 1).
 How many bits do represent one character.
 The default value, 8, sacrifices Unicode handling but is fast and low on memory footprint.
 The value of 18 encompasses I<Basic Multilingual>, I<Supplementary Multilingual> and I<Supplementary Ideographic> planes.
+See also L</UNICODE SUPPORT>
 
 =back
 
-=method hash($string)
+=method hash($octets)
 
-Parses C<$string> and returns a hash reference where keys are the hashed tokens and values are their respective count.
+Parses C<$octets> and returns a hash reference where keys are the hashed tokens and values are their respective count.
+C<$octets> are assumed to represent UTF-8 string unless L<Text::SpeedyFx> is instantiated with L</$bits> == 8
+(which forces Latin-1 mode, see L</UNICODE SUPPORT>).
 Note that this is the slowest form due to the (computational) complexity of the Perl hash structure itself:
-C<hash_fv()> is up to 400% faster, while C<hash_min()> is up to 420% faster.
+C<hash_fv()> is 350% faster, while C<hash_min()> is up to 400% faster.
 
-=method hash_fv($string, $n)
+=method hash_fv($octets, $n)
 
-Parses C<$string> and returns a feature vector (string of bits) with length C<$n>.
+Parses C<$octets> and returns a feature vector (string of bits) with length C<$n>.
 C<$n> is supposed to be a multiplier of 8, as the length of the resulting feature vector is C<ceil($n / 8)>.
 See the included utilities L<cosine_sim> and L<uniq_wc>.
 
-=method hash_min($string)
+=method hash_min($octets)
 
-Parses C<$string> and returns the hash with the lowest value.
+Parses C<$octets> and returns the hash with the lowest value.
 Useful in L<MinHash|http://en.wikipedia.org/wiki/MinHash> implementation.
 See also the included L<minhash_cmp> utility.
 
@@ -87,11 +113,11 @@ The test platform configuration:
 * Perl v5.16.2 (installed via L<perlbrew>);
 * F<enwik8> from the L<Large Text Compression Benchmark|https://cs.fit.edu/~mmahoney/compression/text.html>.
 
-                      Rate    hash hash_min_utf8  hash_fv  hash_min
-    hash           18.9 MB/s    --          -66%     -80%      -81%
-    hash_min_utf8  55.4 MB/s  193%            --     -41%      -44%
-    hash_fv        94.1 MB/s  397%           70%       --       -4%
-    hash_min       98.1 MB/s  419%           77%       4%        --
+                     Rate     hash  hash_min_utf8  hash_fv  hash_min
+    hash           19.5 MB/s    --           -68%     -78%      -80%
+    hash_min_utf8  60.2 MB/s  209%             --     -34%      -40%
+    hash_fv        90.5 MB/s  364%            50%       --       -9%
+    hash_min       99.6 MB/s  411%            66%      10%        --
 
 =head1 REFERENCES
 
@@ -99,5 +125,6 @@ The test platform configuration:
 * L<Extremely Fast Text Feature Extraction for Classification and Indexing|http://www.hpl.hp.com/techreports/2008/HPL-2008-91R1.pdf> by L<George Forman|http://www.hpl.hp.com/personal/George_Forman/> and L<Evan Kirshenbaum|http://www.kirshenbaum.net/evan/index.htm>
 * L<MinHash — выявляем похожие множества|http://habrahabr.ru/post/115147/>
 * L<Фильтр Блума|http://habrahabr.ru/post/112069/>
+* L<cosine_sim>, L<minhash_cmp> and L<uniq_wc> utilities from this distribution
 
 =cut
