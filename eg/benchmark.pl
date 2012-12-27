6caaf09f7b3378633524e15279cb654c4d084391
Stanislaw Pusep <creaktive@gmail.com>
2012-12-27 08:47:05 -0200

    make use of PerlIO::mmap layer

--- eg/benchmark.pl
+++ eg/benchmark.pl
@@ -1,21 +1,24 @@
 #!/usr/bin/env perl
 use autodie;
-use bytes;
 use strict;
+use utf8;
 use warnings;
 
 use Benchmark qw(cmpthese :hireswallclock);
-use File::Map qw(map_file);
 use Text::SpeedyFx;
 
-map_file
-    my $data,
-    q(enwik8);
+my $data;
+{
+    local $/ = undef;
+    open my $fh, q(<:mmap), q(enwik8);
+    $data = <$fh>;
+    close $fh;
+}
 
 my $sfx = Text::SpeedyFx->new(1, 8);
 
 cmpthese(10 => {
     hash        => sub { $sfx->hash($data) },
-    hash_fv     => sub { $sfx->hash_fv($data, 100) },
+    hash_fv     => sub { $sfx->hash_fv($data, 1024) },
     hash_min    => sub { $sfx->hash_min($data) },
 });
