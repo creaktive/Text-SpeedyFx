e36d65985ca1fd05aba3943ffacf57e7e110cb3f
Stanislaw Pusep <creaktive@gmail.com>
2012-12-29 11:26:38 -0200

    added murmur to benchmark for comparison

--- eg/benchmark.pl
+++ eg/benchmark.pl
@@ -5,22 +5,30 @@ use utf8;
 use warnings;
 
 use Benchmark qw(cmpthese :hireswallclock);
+use Digest::MurmurHash qw(murmur_hash);
 use Text::SpeedyFx;
 
-my $data;
-{
+my $data = do {
     local $/ = undef;
     open my $fh, q(<:mmap), q(enwik8);
-    $data = <$fh>;
-    close $fh;
-}
+    <$fh>;
+};
 
 my $sfx_latin1  = Text::SpeedyFx->new(1, 8);
 my $sfx         = Text::SpeedyFx->new(1);
 
 cmpthese(10 => {
     hash            => sub { $sfx_latin1->hash($data) },
+    hash_utf8       => sub { $sfx->hash($data) },
     hash_fv         => sub { $sfx_latin1->hash_fv($data, 10240) },
     hash_min        => sub { $sfx_latin1->hash_min($data) },
     hash_min_utf8   => sub { $sfx->hash_min($data) },
+    murmur_utf8     => sub { tokenize($data) },
 });
+
+sub tokenize {
+    my $fv;
+    ++$fv->{murmur_hash(lc $1)}
+        while $_[0] =~ /(\w+)/gx;
+    return $fv;
+}
